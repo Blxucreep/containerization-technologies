7997c53568399ee47c7802d6d06216b94e70b9a8
Blxucreep <LoevanLqc@outlook.com>
2024-02-16 17:12:43 +0100

    volume part

--- TD 5/up.sh
+++ TD 5/up.sh
@@ -1,5 +1,11 @@
 # up.sh
 #!/bin/bash
 
+# create the my-tiny-network if it doesn't exist
+docker network create my-tiny-network || true
+
+# create the db-vol if it doesn't exist
+docker volume create db-vol || true
+
 docker run -d -p 8080:8080 --name app --network my-tiny-network app
-docker run -d -p 5432:5432 --name db --network my-tiny-network db
+docker run -d -p 5432:5432 --name db --network my-tiny-network -v db-vol:/data db
