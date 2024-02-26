be1491f68fa4e513e213c72c29a6bac1fef1502d
Blxucreep <LoevanLqc@outlook.com>
2024-02-26 17:03:42 +0100

    adding environment variable 'PGDATA' when launching containers

--- TD 5/up.sh
+++ TD 5/up.sh
@@ -8,4 +8,4 @@ docker network create my-tiny-network || true
 docker volume create db-vol || true
 
 docker run -d -p 8080:8080 --name app --network my-tiny-network app
-docker run -d -p 5432:5432 --name db --network my-tiny-network -v db-vol:/data db
+docker run -d -p 5432:5432 --name db --network my-tiny-network -v db-vol:/data -e PGDATA=/data db
